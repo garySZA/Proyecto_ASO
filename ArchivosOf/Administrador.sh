89e825c3bfbd171f1a0771c20d61e7e091f85b31
garySZA <garylvlsoliz@gmail.com>
2021-01-08 16:44:47 -0400

    modificacion del script, se aniadieron los comandos necesarios para el arch leerform2.c

--- ArchivosOf/Administrador.sh
+++ ArchivosOf/Administrador.sh
@@ -3,6 +3,7 @@
 #      BLOQUE DE COMPILACION
 
 gcc index.c -o index
+gcc leerform3.c -o leerform2
 gcc leerform3.c -o leerform3
 gcc leerform5.c -o leerform5
 gcc leerform6.c -o leerform6
@@ -12,6 +13,7 @@ gcc leerform8.c -o leerform8
 #      BLOQUE QUE OTORGA PERMISOS A LOS EJECUTABLES
 
 chmod ugo+s index
+chmod ugo+s leerform2
 chmod ugo+s leerform3
 chmod ugo+s leerform5
 chmod ugo+s leerform6
@@ -21,6 +23,7 @@ chmod ugo+s leerform8
 #      BLOQUE ENCARGADO DE MOVER LOS EJECUTABLES A DESTINO YA CON PERMISOS LISTOS PARA SER UTILIZADOS 
 
 mv index /srv/www/cgi-bin/
+mv leerform2 /srv/www/cgi-bin/
 mv leerform3 /srv/www/cgi-bin/
 mv leerform5 /srv/www/cgi-bin/
 mv leerform6 /srv/www/cgi-bin/
